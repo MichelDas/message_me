b8dc0c2d1ccb0bfc870443838f24e997dab49f94
michel <micheldascse01@gmail.com>
2020-02-03 17:27:25 +0900

    Before starting action cable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Sudipta", password: "password")
+User.create(username: "Sazzad", password: "password")
+User.create(username: "Asif", password: "password")
+User.create(username: "Joyeta", password: "password")
+User.create(username: "Faisal", password: "password")
+
+Message.create(body: "Can we have a coffee", user: User.last)
+Message.create(body: "You seem nice", user: User.last)
+Message.create(body: "fine thank you", user: User.last)
